34b6ba6de2451ff77daf008c59a1eb38f5ce9601
Christian Schmitt <c.schmitt@envisia.de>
2018-06-01 14:32:13 +0200

    WIP: added a module-info.java

---
+++ netty-reactive-streams-http/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.typesafe.netty.http {
+    requires netty.codec.http;
+    requires reactive.streams;
+    requires rt;
+    requires netty.codec;
+    requires netty.buffer;
+    requires netty.common;
+    requires com.typesafe.netty;
+    requires netty.transport;
+    exports com.typesafe.netty.http;
+}
---
+++ netty-reactive-streams/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+
+module com.typesafe.netty {
+    exports com.typesafe.netty;
+    requires netty.transport;
+    requires netty.common;
+    requires reactive.streams;
+    requires rt;
+}
